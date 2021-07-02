f72f371c08d47718585fd03a1ac3b2799920119c
Paul Veliz <paulsystem32@gmail.com>
2021-07-02 11:45:13 -0600

    Quitting unused imports

--- src/main/java/com/pauvel/crudspringboot/Controller/Controlador.java
+++ src/main/java/com/pauvel/crudspringboot/Controller/Controlador.java
@@ -1,24 +1,18 @@
 package com.pauvel.crudspringboot.Controller;
 
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
-import com.pauvel.crudspringboot.Helpers.ConfigHelper;
 import com.pauvel.crudspringboot.InterfaceService.IPersonaService;
 import com.pauvel.crudspringboot.Model.Persona;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
