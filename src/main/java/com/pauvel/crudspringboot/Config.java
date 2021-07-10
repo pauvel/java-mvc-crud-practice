5e68df9d71574356cbafe6f81721e315ac3045ac
Paul Veliz <paulsystem32@gmail.com>
2021-07-10 15:49:30 -0600

    Configure locale resolver & Interceptor

---
+++ src/main/java/com/pauvel/crudspringboot/Config.java
@@ -0,0 +1,39 @@
+package com.pauvel.crudspringboot;
+
+import java.util.Locale;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+@Configuration
+public class Config implements WebMvcConfigurer{
+
+    @Bean
+    public LocaleResolver localeResolver(){
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(Locale.US);
+        return slr;
+    }
+
+    /**
+     * Interceptor for lang param
+     * Allows for changing the current locale on every request.
+     */
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor(){
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry){
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+    
+}
